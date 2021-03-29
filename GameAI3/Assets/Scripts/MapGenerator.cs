6010e257ffee10affd1acf887b05f6a732bc95c4
Sheryl Mantik <s3693591@student.rmit.edu.au>
2021-03-29 22:38:52 +1100

    using perlin noise for procedural generation

--- GameAI3/Assets/Scripts/MapGenerator.cs
+++ GameAI3/Assets/Scripts/MapGenerator.cs
@@ -10,93 +10,51 @@ public class MapGenerator : MonoBehaviour {
     public Vector3Int tmapSize;
 
     public Tilemap baseLayer;
-    public Tilemap topLayer;
+    public Tilemap obstacleLayer;
 
     // base layer tiles
     public Tile grass;
-    public Tile water;
 
-    // top layer
+    // obstacle layer
+    [Range(0f,100f)]
+    public float scale = 15f;
     public Tile tree;
-    [Range(1,100)]
-    public int treeChance;
-    [Range(1,100)]
-    public int treeNearbyChance; // chance of putting tree if there is another tree nearby
-    // public int treeNum; // ignore this for now
-    private int treePlacedNum;
-
     public Tile rock;
-    [Range(1,100)]
-    public int rockChance;
-    [Range(1,100)]
-    public int rockNearbyChance; // chance of putting rock if there is another rock nearby
-    // public int rockNum; // ignore this for now
-    private int rockPlacedNum;
+    public Tile water;
 
     // Start is called before the first frame update
     void Awake() {
+        // helper variables to make generated map different every time
+        float offsetX = Random.Range(0f,9999f);
+        float offsetY = Random.Range(0f,9999f);
+
         // initialize map
         for(int x = 0; x < mapWidth; x++){
             for(int y = 0; y < mapHeight; y++){
                 // set base
-                baseLayer.SetTile(new Vector3Int(-x + mapWidth/2, -y + mapHeight/2, 0), grass);
+                int xTmap = -x + mapWidth/2;
+                int yTmap = -y + mapHeight/2;
+                int zTmap = 0;
+                baseLayer.SetTile(new Vector3Int(xTmap, yTmap, zTmap), grass);
                 
-                // set top
+                // set obstacle
                 // check if currentPos is Player starting point
-                bool playerStartPoint = (-x + mapWidth/2 == 0 && -y + mapHeight/2 == 0) ? true : false;
-                int randomNum = Random.Range(1,100);
+                bool playerStartPoint = (xTmap == 0 && yTmap == 0) ? true : false;
                 
                 if (!playerStartPoint){
-                    // check surroundings, if there's tree around
-                    if (TileNearby(x, y, tree, topLayer)){
-                        if (randomNum < treeNearbyChance+1){
-                            topLayer.SetTile(new Vector3Int(-x + mapWidth/2, -y + mapHeight/2, 0), tree);
-                            treePlacedNum++;
-                        }
-                    } else {
-                        if (randomNum < treeChance+1){
-                            topLayer.SetTile(new Vector3Int(-x + mapWidth/2, -y + mapHeight/2, 0), tree);
-                            treePlacedNum++;
-                        }
-                    }
+                    float xCoord = -(float)x/mapWidth * scale + offsetX;
+                    float yCoord = -(float)y/mapHeight * scale + offsetY;
+                    float sample = Mathf.PerlinNoise(xCoord, yCoord); // value would be between 0 and 1 inclusive
 
-                    if (topLayer.GetTile<Tile>(new Vector3Int(-x + mapWidth/2, -y + mapHeight/2, 0)) != tree){
-                        // check surroundings, if there's rock around
-                        if (TileNearby(x, y, rock, topLayer)){
-                            if (randomNum < rockNearbyChance+1){
-                                topLayer.SetTile(new Vector3Int(-x + mapWidth/2, -y + mapHeight/2, 0), rock);
-                                rockPlacedNum++;
-                            }
-                        } else {
-                            if (randomNum < rockChance+1){
-                                topLayer.SetTile(new Vector3Int(-x + mapWidth/2, -y + mapHeight/2, 0), rock);
-                                rockPlacedNum++;
-                            }
-                        }
-                    }
-                }
-                
-            }
-        }
-    }
-
-    private bool TileNearby(int x, int y, Tile tile, Tilemap layer){
-        bool tileNearby = false;
-        // TODO: Fix later, does not need to check every tile
-        for(int a = x-1; a < x+1; a++){
-            for(int b = y-1; b < y+1; b++){
-                Tile tempTile = layer.GetTile<Tile>(new Vector3Int(-a + mapWidth/2, -b + mapHeight/2, 0));
-                if (!(a == x && b == y)){
-                    if (tempTile != null){
-                        if (tempTile == tile){
-                            tileNearby = true;
-                            break;
-                        }
+                    if (sample >= 0f && sample < 0.2f){
+                        obstacleLayer.SetTile(new Vector3Int(xTmap, yTmap, 0), rock);
+                    } else if (sample > 0.2f && sample < 0.35f || sample > 0.65f && sample <= 0.7f  ){
+                        obstacleLayer.SetTile(new Vector3Int(xTmap, yTmap, 0), tree);
+                    } else if (sample > 0.7f && sample <= 1f){
+                        obstacleLayer.SetTile(new Vector3Int(xTmap, yTmap, 0), water);
                     }
                 }
             }
         }
-        return tileNearby;
-    }
-    
+    }    
 }
