62ea65fffbd27f271664f10d644b22523f86289a
Sheryl Mantik <s3693591@student.rmit.edu.au>
2021-03-31 22:49:39 +1100

    added radius check from starting point

--- GameAI3/Assets/Scripts/MapGenerator.cs
+++ GameAI3/Assets/Scripts/MapGenerator.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+/****
+PROCEDURAL GENERATION
+to do:
+- clear out obstacle layer at a specified radius
+- create streams on a different layer
+- check if at the starting point, player can get to the 4 corners of the board -- uses pathfinding
+****/
+
 public class MapGenerator : MonoBehaviour {
     public int mapWidth;
     public int mapHeight;
@@ -14,13 +22,18 @@ public class MapGenerator : MonoBehaviour {
 
     // base layer tiles
     public Tile grass;
+    public Tile water;
 
     // obstacle layer
     [Range(0f,100f)]
     public float scale = 15f;
     public Tile tree;
     public Tile rock;
-    public Tile water;
+
+    [Range(0,20)]
+    public int startPointRadius=5;
+
+    private Pathfinding pf; // to access pathfinding functions
 
     // Start is called before the first frame update
     void Awake() {
@@ -35,26 +48,32 @@ public class MapGenerator : MonoBehaviour {
                 int xTmap = -x + mapWidth/2;
                 int yTmap = -y + mapHeight/2;
                 int zTmap = 0;
+
                 baseLayer.SetTile(new Vector3Int(xTmap, yTmap, zTmap), grass);
                 
                 // set obstacle
-                // check if currentPos is Player starting point
-                bool playerStartPoint = (xTmap == 0 && yTmap == 0) ? true : false;
-                
-                if (!playerStartPoint){
+                if (!InStartPointRadius(xTmap, yTmap)){
                     float xCoord = -(float)x/mapWidth * scale + offsetX;
                     float yCoord = -(float)y/mapHeight * scale + offsetY;
                     float sample = Mathf.PerlinNoise(xCoord, yCoord); // value would be between 0 and 1 inclusive
 
                     if (sample >= 0f && sample < 0.2f){
                         obstacleLayer.SetTile(new Vector3Int(xTmap, yTmap, 0), rock);
-                    } else if (sample > 0.2f && sample < 0.35f || sample > 0.65f && sample <= 0.7f  ){
+                    } else if (sample > 0.2f && sample < 0.35f){ //|| sample > 0.65f && sample <= 0.7f  ){
                         obstacleLayer.SetTile(new Vector3Int(xTmap, yTmap, 0), tree);
-                    } else if (sample > 0.7f && sample <= 1f){
-                        obstacleLayer.SetTile(new Vector3Int(xTmap, yTmap, 0), water);
                     }
                 }
             }
         }
-    }    
+    }
+
+    bool InStartPointRadius(int x, int y){
+        bool inRadius = false;
+
+        if (Mathf.Abs(x) + Mathf.Abs(y) <= startPointRadius){
+            inRadius = true;
+        }
+
+        return inRadius;
+    }  
 }
