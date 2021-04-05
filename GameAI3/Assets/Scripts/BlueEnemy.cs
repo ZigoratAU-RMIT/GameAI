4f54ef5f2e89649ffdb5ffef3cdf4ee2a6ed4abe
Mohammad Farshidmehr <s3308107@student.rmit.edu.au>
2021-04-05 23:46:48 +1000

    Add BlueEnemy

---
+++ GameAI3/Assets/Scripts/BlueEnemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueEnemy : MonoBehaviour
+{
+    public float blinking = 0.4f;
+    public int delayShow = 100;
+    private GameObject blueEnemy;
+    private int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //find the spawnObjects
+        blueEnemy = GameObject.Find("BlueEnemy");
+        InvokeRepeating("DelayShow", 0, blinking);
+        float locX = Random.Range(-10f, 10f);
+        float locY = Random.Range(-10f, 10f);
+        blueEnemy.transform.position = new Vector2(locX, locY);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if (counter == delayShow)
+        {
+            CancelInvoke("DelayShow");
+        }
+    }
+
+    void DelayShow()
+    {
+        if (blueEnemy.activeSelf)
+        {
+            blueEnemy.SetActive(false);
+        }
+        else
+        {
+            blueEnemy.SetActive(true);
+        }
+    }
+}
---
+++ GameAI3/Assets/Scripts/RedEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedEnemy : MonoBehaviour
+{
+  public int delayShow = 150;
+  public float blinking = 0.3f;
+  private GameObject redEnemy;
+  private int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+      //find the spawnObjects
+      redEnemy = GameObject.Find("RedEnemy");
+      InvokeRepeating("DelayShow", 0 , blinking);
+      float locX = Random.Range(-10f,10f);
+      float locY = Random.Range(-10f,10f);
+      redEnemy.transform.position = new Vector2(locX,locY);          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      counter++;
+      if (counter == delayShow)
+      {
+        CancelInvoke("DelayShow");  
+      }
+    }
+    
+    void DelayShow() {
+    if(redEnemy.activeSelf){
+      redEnemy.SetActive(false);
+    }
+    else{
+      redEnemy.SetActive(true);
+    }
+  }
+}
