9ca18bfd38ad81e78532d31e913a964f72ba9183
Mohammad Farshidmehr <s3308107@student.rmit.edu.au>
2021-04-11 09:59:13 +1000

    Add Blue and RED EnemyPursuitBehaviour

---
+++ GameAI3/Assets/Scripts/RedEnemyPursuitBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedEnemyPursuitBehaviour : MonoBehaviour
+{
+    public Transform player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float moveSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        moveEnemy(movement);
+    }
+
+    void moveEnemy(Vector2 direction)
+    {
+        if (player.position != transform.position)
+        {
+            rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+        }
+    }
+}
