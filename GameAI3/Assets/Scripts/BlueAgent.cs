3371eac7f0ef346db1bb1a811850c83572b9ca98
Sheryl Mantik <s3693591@student.rmit.edu.au>
2021-04-11 14:03:25 +1000

    added flee behaviour and created agent to incorporate different behaviours

---
+++ GameAI3/Assets/Scripts/BlueAgent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueAgent : MonoBehaviour
+{
+    public Transform player;
+    public float maxSpeed = 15f;
+    private GameObject gameObj;
+    private Rigidbody2D body;
+    private FleeBehavior flee;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObj = GameObject.Find("BlueAgent");
+        body = GetComponent<Rigidbody2D>();
+        flee = new FleeBehavior();
+        float locX = Random.Range(-10f, 10f);
+        float locY = Random.Range(-10f, 10f);
+        gameObj.transform.position = new Vector2(locX, locY);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // finite state machine
+        float distance = Vector2.Distance(transform.position, player.position); // should translate this to world tiles
+        if (distance < 5)
+        {
+            gameObj.transform.position = flee.calculateMove(transform.position, player.position, body, maxSpeed);
+        }
+    }
+}
---
+++ GameAI3/Assets/Scripts/FleeBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleeBehavior : MonoBehaviour
+{
+    public Vector2 calculateMove(Vector2 agentPos, Vector2 target, Rigidbody2D body, float maxSpeed){
+        Vector2 desiredVelocity = -(target - agentPos);
+        desiredVelocity = desiredVelocity.normalized * maxSpeed;
+
+        Vector2 steering = desiredVelocity - body.velocity;
+        steering = Vector2.ClampMagnitude(steering, maxSpeed);
+         
+        body.velocity = Vector2.ClampMagnitude(body.velocity + steering, maxSpeed);
+        return body.velocity.normalized;
+    }
+
+}
