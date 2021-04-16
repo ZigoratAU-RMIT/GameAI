8040d43f30858bb83a07666ba39870e25dc0ea68
CurtisMaunder <79820212+CurtisMaunder@users.noreply.github.com>
2021-04-16 22:23:23 +1000

    Behaviour Scripts
    Behaviour Scripts for Seek and Wander

---
+++ GameAI3/Assets/Scripts/Seek.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : MonoBehaviour{
+
+    private Vector2 desiredVelocity;
+    private Vector2 steering;
+
+    public Vector2 Movement(Vector2 position, Vector2 targetPosition, Vector2 bodyVelocity, int speed){
+        desiredVelocity = targetPosition - position;
+        desiredVelocity = desiredVelocity.normalized * speed;
+
+        steering = desiredVelocity - bodyVelocity;
+        steering = Vector2.ClampMagnitude(steering, speed);
+        steering /= 15f;
+
+        return steering;
+    }
+}
---
+++ GameAI3/Assets/Scripts/Wander.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour {
+    private Vector2 steering;
+
+    //Wander
+    private Vector2 circleCentre;
+    private Vector2 displacement;
+    private Vector2 wanderForce;
+
+    public Vector2 Movement(Vector2 bodyVelocity, int speed){
+        circleCentre = bodyVelocity;
+        circleCentre = circleCentre.normalized * 2;
+
+        displacement = new Vector2(0, -1);
+        displacement = displacement.normalized * 2;
+
+        int heading = Random.Range(0, 360);
+
+        displacement.x = Mathf.Cos(heading);
+        displacement.y = Mathf.Sin(heading);
+
+        wanderForce = circleCentre + displacement;
+
+        steering = wanderForce - bodyVelocity;
+        steering = Vector2.ClampMagnitude(steering, speed);
+        steering /= 15f;
+
+        return steering;
+    }
+}
